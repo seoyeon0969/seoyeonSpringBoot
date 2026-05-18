79b9667e4e729230ad286ea4b3daa65abe318177
seoyeon0969 <seoyeon0969@gmail.com>
2026-05-18 10:51:33 +0900

    0513 Spring

--- first-project/src/main/java/com/example/first/project/repository/ArticleRepository.java
+++ first-project/src/main/java/com/example/first/project/repository/ArticleRepository.java
@@ -4,5 +4,9 @@ import com.example.first.project.entity.Article;
 
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.ArrayList;
+
 public interface ArticleRepository extends CrudRepository<Article, Long> {
+    @Override
+    ArrayList<Article> findAll();
 }
