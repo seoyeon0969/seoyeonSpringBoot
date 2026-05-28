b05fc3489936a7b77d2ffe67f71538b3d009a343
seoyeon0969 <seoyeon0969@gmail.com>
2026-05-28 22:00:42 +0900

    0527 Spring

--- first-project/src/main/java/com/example/first/project/ArticleController.java
+++ first-project/src/main/java/com/example/first/project/ArticleController.java
@@ -35,7 +35,7 @@ public class ArticleController {
         Article saved = articleRepository.save(article);
         log.info(saved.toString());
         // System.out.println(saved.toString());
-        return "";
+        return "redirect:/articles/" + saved.getId();
     }
     @GetMapping("/articles/{id}")
     public String show(@PathVariable Long id, Model model){
@@ -55,5 +55,26 @@ public class ArticleController {
         model.addAttribute("articleList",articleEntityList);
         // 3. 뷰 페이지 설정하기
         return "articles/index";
+    }
+    @GetMapping("/articles/{id}/edit")
+    public String edit(@PathVariable Long id, Model model) {
+
+        Article articleEntity = articleRepository.findById(id).orElse(null);
+        model.addAttribute("article",articleEntity);
+        return "articles/edit";
+    }
+    @PostMapping("/articles/update")
+    public String update(ArticleForm form) {
+        log.info (form.toString());
+        //1. DTO를 엔티티로 변환하기
+        Article articleEntity = form.toEntity();
+        log.info (articleEntity.toString());
+        //2. 엔티티를 DB에 저장하기
+        Article target = articleRepository.findById(articleEntity.getId()).orElse(null);
+        if (target != null) {
+            articleRepository.save(articleEntity);
+        }
+        //3. 수정 결과 페이지로 리다이렉트하기
+        return "redirect:/articles/" + articleEntity.getId();
     }
 }
